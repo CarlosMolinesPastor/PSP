922108c0bcdc7185bcf1365c9efa0c11f400dea9
Carlos Molines Pastor <karlinux47@gmail.com>
2024-01-08 17:11:07 +0100

    Update contentTypes.js and practica1.js

--- Practica1/contentTypes.js
+++ Practica1/contentTypes.js
@@ -1,17 +1,17 @@
 // contentTypes.js
 // Definimos un objeto que contiene los tipos de contenido que acepta el servidor
 const contentTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.jpg': 'image/jpg',
-    // Se pueden añadir mas contenidos como png,etc
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".jpg": "image/jpg",
+  // Se pueden añadir mas contenidos como png,etc
 };
 // Funcion para obtener el tipo de contenido de un archivo
 // Recibe como argumento la extension del archivo
 function getContentType(extname) {
-    // Devolvemos el tipo de contenido del archivo o text/html por defecto
-    return contentTypes[extname] || 'text/html';
+  // Devolvemos el tipo de contenido del archivo o text/html por defecto
+  return contentTypes[extname] || "text/html";
 }
 // Exportamos la funcion como un objeto
 module.exports = getContentType;
