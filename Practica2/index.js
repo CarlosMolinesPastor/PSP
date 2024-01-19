6ad49f21efe63061be4189e38121749be1a82555
Carlos Molines Pastor <karlinux47@gmail.com>
2024-01-19 20:41:25 +0100

    Added Comentaries

--- Practica2/index.js
+++ Practica2/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const mongoose = require('mongoose');
 const url = require("url")
 
+//###################### Creacion de la base de datos ########################
 //Creamos el esquema de la base de datos
 let marvelSchema = new mongoose.Schema({
     nombre: String,
@@ -12,6 +13,7 @@ let marvelSchema = new mongoose.Schema({
 //Creamos el modelo de la coleccion de la base de datos
 let Superheroe = mongoose.model('superheroes', marvelSchema);
 
+//###################### Funciones ########################
 // Función para buscar superheroes y generar la tabla HTML
 // La función recibe un callback que se ejecuta cuando se han obtenido los superheroes
 function buscarSuperheroes(callback) {
@@ -24,25 +26,29 @@ function buscarSuperheroes(callback) {
             callback(err);
             return;
         }
-        // Si no hay error, generamos la tabla HTML con los superheroes con un forEach
+        // Si no hay error, generamos la tabla HTML con los superheroes con un forEach que lle cada linea de la tabla
+        // y la vamos a anadiendo a la variable fila declarada arriba
         superheroes.forEach(superheroe => {
             // Añadimos una fila por cada superheroe
             fila += `<tr><td>${superheroe.nombre}</td><td>${superheroe.superpoder}</td><td>${superheroe.debilidad}</td><td>${superheroe.fuerza}</td></tr>`;
         });
-        // Cerramos la tabla
+        // Por ultimo cerramos la tabla
         fila += '</table>';
         callback(null, fila);
     });
 }
 
-//Declaramos el puerto, el localhost y el servidor
-const port = 3000; 
-
+//###################### Servidor ########################
 const requestListener = function (request,response){
+    //Primera parte declaramos las variables para las busquedas
     let url_parts = url.parse(request.url, true);
     let superHeroe = url_parts.query.nombre;
+    //Segunda parte declaramos las respuestas
+    //Si el superheroe no es null, mostramos el superheroe aunque en la busqueda  le pasamos un posible error a ella
     if (superHeroe != null){
+        //Busqueda en la base de datos mongo por superheroe
         Superheroe.find({nombre: superHeroe}, (err,superheroes) =>{
+            //Si hay error o no hay resultados, mostramos el error
             if(err || superheroes.length == 0){
             response.setHeader("Content-Type", "text/html");
             response.writeHead(500);
@@ -50,32 +56,44 @@ const requestListener = function (request,response){
             response.end;
             return;
             }
+            //Si hay resultados, mostramos el superheroe primero y luego sus caracteristicas
             let superheroe = superheroes[0];
             let nombre = superheroe.nombre;
             let lista = `<p>Superpoder: ${superheroe.superpoder}</p><p>Debilidad: ${superheroe.debilidad}</p><p>Fuerza: ${superheroe.fuerza}</p>`;
                         response.setHeader("Content-Type", "text/html");
+            //Respondemos con el nombre del superheroe y sus caracteristicas
             response.writeHead(200);
             response.write(`<h1>Super Heroe: ${nombre}</h1>`);
             response.end(lista);
         });
+    //Si el superheroe es null, es decir que no se ha realizado ninguna busqueda, mostramos todos los superheroes
     }else {
+        //Llamamos a la funcion buscarSuperheroes y le pasamos un posible error 
+        // y la lista de superheroes que compone el callback
         buscarSuperheroes((err,lista) => {
+        //Si hay error, mostramos el error por consola aunque podr;iamos mostrarlo por pantalla mandando un mensaje 500
         if (err){
             console.log(err);
             return;
         }
         response.setHeader("Content-Type", "text/html");
         response.writeHead(200);
+        //Respondemos con la lista de superheroes
         response.write("<h1>Super Heroes</h1>");
         response.end(lista);
     });
     }
 }
 
+//Declaramos el puerto, el localhost y el servidor
+const port = 3000; 
+
 const server = http.createServer(requestListener);
 server.listen(port);
 console.log('Server running at http://127.0.0.1:3000/');
 
+//###################### Conexion a la base de datos ########################
+// Evitamos los mensajes de error de deprecated #### Buscado en internet
 mongoose.set('strictQuery', true);
 // Conectamos a la base de datos por el puerto especificado.
 mongoose.connect('mongodb://localhost:27017/Marvel');
